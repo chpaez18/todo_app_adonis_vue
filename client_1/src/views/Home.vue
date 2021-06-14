5a3fbef1cc6a04cf10eb159c85405310c1e01a65
Christian <christianpaez1029@gmail.com>
2021-06-14 13:35:53 -0400

    adicion tailwindcss

--- client_1/src/views/Home.vue
+++ client_1/src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <p class="text-purple-500">Tailwindcss is working</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
+
 }
 </script>
